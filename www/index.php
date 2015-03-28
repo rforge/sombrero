84b9ad5f3d5697ab322ad26f8ac231265eacd051
tuxette <tuxette@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-03-28 09:42:03 +0000

    SOMbrero version 1.0 (first CRAN release)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sombrero@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -135,42 +135,24 @@ echo $contents; } ?>
 
   <div id="outer-content">
     <div id="intro">
-<ul>
-<li>Find the <strong>project summary page</strong> <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/">here</a>. </li>
-<li>Find the <strong>project webpage</strong> with various documents <a href="http://tuxette.nathalievilla.org/?p=1099&lang=en">here</a>.</li>
-<li>Find a Web User Interface, based on <a href="http://www.rstudio.com/shiny/">shiny</a>, <a href="http://shiny.nathalievilla.org/sombrero">here</a> (SOMbrero WUI, v0.2-2 based on SOMbrero v0.5).</li>
-<li>Download the <a href="./SOMbrero-manual.pdf">manual</a>.</li><li><strong>Package's vignettes</strong> (detailed use cases) are also available for <a href="./doc-numericSOM.html">numeric data</a>,
-for <a href="./doc-korrespSOM.html">contingency tables</a> and for <a href="./doc-relationalSOM.html">dissimilarity data</a>.
-An overview of the package is given in the <a href="./doc-SOMbrero-package.html">package's main vignette</a>.</li>
-</ul>
-</div>
-<div id="outer-content">
-    <div id="intro">
     <h2>Install SOMbrero</h2>
-Installation is done by first installing some required packages from CRAN:
+		SOMbrero is now on CRAN:
 <pre>
-install.packages(c("wordcloud","igraph","RColorBrewer","scatterplot3d","knitr","shiny"),
-                 repos="http://cran.r-project.org/")
+install.packages("SOMbrero")
 </pre>
-and then install SOMbrero directly from R-Forge:
-<pre>
-install.packages("SOMbrero", repos="http://R-Forge.R-project.org")
-</pre>
-or, alternatively, 
-<pre>
-install.packages("SOMbrero", repos="http://R-Forge.R-project.org", type="source")
-</pre>
-if you are using a Mac.<br>
-In case installation from R-Forge does not work, download the package <a href="https://r-forge.r-project.org/R/?group_id=1707">here</a> and use installation from ZIP file in Windows GUI or for Unix users, the command line <code>R CMD INSTALL SOMbrero_XXX.tar.gz</code><br><br>
-Do not hesitate to contact <a href="mailto:tuxette[AT]nathalievilla.org">tuxette</a> if you have a problem during the installation.
-</div>
+		should be enough to install the package and its dependencies on your computer. 
+		<br><br>
+		Do not hesitate to contact <a href="mailto:tuxette[AT]nathalievilla.org">tuxette</a> if you have a problem during the installation.
+	</div>
+	</div>
     <div id="content">
 <p style="color:#A558B4"><em>Remember that this package has been developped only by girls. Default colors may not be suited for men.</em></p>
+		</div>
 
 To cite the package, please use:
 <ul>
 <li>
-Villa-Vialaneix N., Bendhaiba L., Olteanu M. (2013) <em>SOMbrero: SOM Bound to Realize Euclidean and Relational Outputs</em>. R package version 0.5.</li>
+Villa-Vialaneix N., Bendhaiba L., Olteanu M. (2015) <em>SOMbrero: SOM Bound to Realize Euclidean and Relational Outputs</em>. R package version 1.0.</li>
 <li>Olteanu M., Villa-Vialaneix N. (2015) On-line relational and multiple relational SOM. <em>Neurocomputing</em>, <strong>147</strong>, 15-30.</li>
 <li>Olteanu M., Villa-Vialaneix N., Cottrell M. (2012) On-line relational SOM for dissimilarity data. <em>Advances in Self-Organizing Maps (Proceedings of WSOM 2012, Santiago, Chili, 12-14 decembre 2012), Estevez P., Principe  J., Zegers P., Barreto G. (eds.), Advances in Intelligent Systems and Computing series</em>, Berlin/Heidelberg:  Springer Verlag, <b>198</b>, 13-22.</li></ul>
 </div>
